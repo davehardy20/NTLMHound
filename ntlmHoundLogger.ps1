f6c412496f037e039e957a9f6996187f2f09c0ab
Nikolay Salnikov <niksa@microsoft.com>
2017-02-26 21:32:02 +0100

    Initial publishing

---
+++ ntlmHoundLogger.ps1
@@ -0,0 +1,135 @@
+﻿<#
+.SYNOPSIS
+    NTLMHound is a toolkit for discovering LM and NTLMv1 usage within Active Directory domain.
+.DESCRIPTION
+    NTLM Hound Logger is a script for NTLM ETW Log collection
+.PARAMETER Mode
+    Mode for running the script. Possible values:
+    1. Enable ETW tracing for a defined timeframe and collect logs
+    2. Disable ETW tracing and collect logs
+.PARAMETER Timeframe
+    Time frame in minutes for collecting ETW logs 
+.PARAMETER Logpath
+    A path to a location where the resulting ETW logs will be stored.
+.EXAMPLE
+    Enable NTLM ETW Log collection for 5 minutes and put the resulting logs to c:\ntlmlogs
+    C:\PS> .\ntlmHoundLogger.ps1 -Mode 1 -Timeframe 5 -Logpath c:\ntlmlogs
+    
+    Disable NTLM ETW Log collection 
+    C:\PS> .\ntlmHoundLogger.ps1 -Mode 2
+
+.NOTES
+    Author: Nikolay Salnikov
+    Date:   February 26, 2017
+    Version: 1.1
+#>
+
+
+Param(
+    [Parameter (HelpMessage="Choose Mode for running the script. 1 - enable ETW tracing, 2 - disable ETW tracing")]
+    [ValidateNotNullOrEmpty()]
+    [string]$Mode = '1',
+    [Parameter (HelpMessage="Time frame in minutes for ETW tracing")]
+    [ValidateNotNullOrEmpty()]
+    [string]$Timeframe = "1",
+    [Parameter (HelpMessage="A file path to a folder where to store the resulting files")]
+    [ValidateNotNullOrEmpty()]
+    [string]$Logpath = ".\ETL"
+)
+
+
+
+Function Start-Countdown 
+{   
+    Param(
+        [Int32]$Seconds = 10
+    )
+    ForEach ($Count in (1..$Seconds))
+    {   Write-Progress -Id 1 -Activity "Trace collection is in progress..." -Status "Collecting for $Seconds seconds, $($Seconds - $Count) left" -PercentComplete (($Count / $Seconds) * 100)
+        Start-Sleep -Seconds 1
+    }
+    Write-Progress -Id 1 -Activity "Trace collection is in progress..." -Status "Completed" -PercentComplete 100 -Completed
+}
+
+
+function Test-RegistryValue {
+param (
+
+ [parameter(Mandatory=$true)]
+ [ValidateNotNullOrEmpty()]$Path,
+
+[parameter(Mandatory=$true)]
+ [ValidateNotNullOrEmpty()]$Value
+)
+
+try {
+Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Value -ErrorAction Stop | Out-Null
+ return $true
+ }
+
+catch {
+return $false
+}
+
+}
+
+
+#$registryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\MSV1_0"
+#$regvaluename = "NtLmInfoLevel"
+#$regvalue= 0xc0015003
+#$collect = "n"
+
+if ($mode -eq "1")
+{
+Write-Output("Running mode 1: Enable NTLM ETW tracing for $Timeframe minute(s) and save the resulting logs to $Logpath")
+}
+elseif ($mode -eq "2")
+{
+Write-Output("Running mode 2: Disable ETW tracing")
+}
+else
+{
+Write-Output("Possible options for Mode parameter:")
+Write-Output("1. Enable ETW Tracing")
+Write-Output("2. Disable ETW Tracing")
+}
+
+
+if ($mode -eq "1")
+{
+Start-Process -FilePath ".\tracelog.exe" -ArgumentList "-stop ntlm"
+
+
+while (!(Test-Path $Logpath))
+{
+Write-Output ("The specified path doesn't exist. A new folder will be created.")
+New-Item $Logpath -type directory | Out-Null
+}
+
+Write-Output ("Enabling tracing...")
+Start-Process -FilePath ".\tracelog.exe" -ArgumentList "-kd -rt -start ntlm -guid #5BBB6C18-AA45-49b1-A15F-085F7ED0AA90 -f $LogPath\ntlm.etl -flags 0x15003 -ft 1"
+Write-Output ("Done.")
+
+Write-Output ("Collecting data...")
+Start-Countdown(([int]$Timeframe*60).ToString())
+Write-Output ("Finished.")
+
+Write-Output ("Disabling tracing...")
+Start-Process -FilePath ".\tracelog.exe" -ArgumentList "-stop ntlm"
+Write-Output ("Done.")
+Write-Output ("Find collected logs here: $Logpath")
+
+}
+
+
+elseif ($mode -eq "2")
+{
+Write-Output ("Disabling tracing...")
+Start-Process -FilePath ".\tracelog.exe" -ArgumentList "-stop ntlm"
+Write-Output ("Done.")
+}
+
+
+
+
+
---
+++ ntlmHoundParser.ps1
@@ -0,0 +1,62 @@
+﻿<#
+.SYNOPSIS
+    NTLMHound is a toolkit for discovering LM and NTLMv1 usage within an Active Directory domain.
+.DESCRIPTION
+    NTLMHoundParser is a script for preparing a report about NTLM usage based on the data in collected ETW traces (see ntlmHoundLogger.ps1).
+.PARAMETER Logpath
+    A path to a folder containing collected ETW traces.
+.PARAMETER TmfPath
+    A path to TMF files. The path could be in UNC format. 
+.PARAMETER OutFile
+    A path to a report file.
+.EXAMPLE
+    Parse the collected ETW logs in c:\mylogs using default toolkit TMF files and save the report to ntlmusage.csv
+    C:\PS> .\ntlmHoundParser.ps1 -Logpath "c:\mylogs" -TmfPath ".\tmf" -OutFile ".\ntlmusage.csv"
+   
+
+.NOTES
+    Author: Nikolay Salnikov
+    Date:   February 26, 2017
+    Version: 1.1
+#>
+Param(
+    [ValidateNotNullOrEmpty()]
+    [string]$Logpath = ".\ETL",
+    [ValidateNotNullOrEmpty()]
+    [string]$TmfPath = ".\tmf",
+    [ValidateNotNullOrEmpty()]
+    [string]$OutFile = ".\results.csv"
+
+)
+
+
+New-Item "$Logpath\decoded" -type directory -Force | Out-Null
+Get-ChildItem "$Logpath" -Filter *.etl | 
+Foreach-Object {
+    $fn= $_.FullName
+    $rn = $_.Name
+    $tracefmt = ".\tracefmt.exe" 
+    &$tracefmt $fn -p $TmfPath -o "$Logpath\decoded\$rn.decoded.log" | Out-Null
+}
+
+$regex = 'user\s\"(?<user>[\w|\-|\$_.]+)\",\sdomain\s\"(?<domain>[\w|\-|\$_.]+)\",\sworkstation\s\"(?<workstation>[\w|\-|\$_.]+)\",\sNtRespLen\s0x(?<buflen>\w+),\s'
+
+#$content = gc "$Logpath\decoded\*.decoded.log"
+#$content |?{$_ -match $regex}|%{$matches['domain']+"\"+$matches['user']+";"+$matches['workstation']+";"+$matches['buflen']} | Sort-Object| Get-Unique | Out-File $OutFile
+
+
+gc "$Logpath\decoded\*.decoded.log" |?{$_ -match $regex}|%{$matches['domain']+"\"+$matches['user']+";"+$matches['workstation']+";"+$matches['buflen']} | Sort-Object| Get-Unique | Foreach-Object {
+        # send the current line to output
+        if ($_ -match ";18") 
+        {
+            #Add LM-NTLMv1 tag
+            "$_;LM-or-NTLMv1"
+        }
+        else {
+            #Add NTLMv2 tag
+            "$_;NTLMv2"
+            }
+    } | Out-File $OutFile
+
+
+Write-Output ("Log processing is completed. Find your report here: $OutFile")
